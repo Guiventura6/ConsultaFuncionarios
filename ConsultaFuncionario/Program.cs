897d93b0726463051c26cdf5f21e9fdbeef1c1f6
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-10 21:52:05 -0300

    Projeto Criado.

---
+++ ConsultaFuncionario/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsultaFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
